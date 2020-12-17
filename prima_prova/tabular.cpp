e4771a0e568ca0f6d4aacf6b9c61f2c239dd8817
Tommaso Lamma <lammatommaso@gmail.com>
2020-12-17 17:06:53 +0100

    default

---
+++ prima_prova/tabular.cpp
@@ -0,0 +1,68 @@
+#include<fstream>
+#include<cmath>
+#include<string>
+
+#define N 15
+
+const std::string nl = "\\";
+const std::string NL = nl + nl;
+
+struct Error_Point
+{
+    double V;
+    double e_V;
+    double I;
+    double e_I;
+
+    Error_Point(){}
+    friend std::istream& operator>>(std::istream& is, Error_Point& p)
+    {
+        is>>p.V>>p.e_V>>p.I>>p.e_I;
+	return is;
+    }
+	friend std::ostream& operator<<(std::ostream& os, Error_Point& p)
+    {
+        os<<p.V<<" & "<<p.e_V<<" & "<<p.I<<" & "<<p.e_I<<NL;
+	return os;
+    }
+};
+
+struct Error_Point_Array
+{
+    Error_Point el[N];
+    Error_Point_Array(){}
+
+    friend std::istream& operator>>(std::istream& is, Error_Point_Array& arr)
+    {
+        for(int i=0;i<N;i++)
+	{
+	    is>>arr.el[i];
+	}
+	return is;
+    }
+    friend std::ostream& operator<<(std::ostream& os, Error_Point_Array& arr)
+    {
+		for(int i=0;i<N;i++)
+		{
+			os<<arr.el[i]<<'\n';
+		}
+	}
+};
+
+
+
+int main() 
+{
+   std::ifstream input;
+   input.open("calibrazione/dati.txt");
+   Error_Point_Array pydata;
+   input>>pydata;
+   input.close();
+
+   std::ofstream output;
+   output.open("calibrazione/tabular");
+   output<<pydata;
+   output.close();
+}
+
+
